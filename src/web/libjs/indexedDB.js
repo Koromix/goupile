Koromix/goupile

commit c7626e9584ecfb4bb75e40304106fa35ab4cb763

Solidify promise wrapper code around IndexedDB

src/web/libjs/indexedDB.js
@@ -57,11 +57,18 @@ let idb = new function () {
             if (transaction)
                 throw new Error('Nested transactions are not supported');
 
-            transaction = db.transaction(stores, mode);
+            let p = new Promise((resolve, reject) => {
+                transaction = db.transaction(stores, mode);
+
+                transaction.addEventListener('complete', e => resolve());
+                transaction.addEventListener('abort', e => reject(new Error('Database transaction failure')));
+            });
+
             try {
                 await func();
+                await p;
             } catch (err) {
-                if (!aborted)
+                if (!aborted && transaction.error == null)
                     transaction.abort();
 
                 throw err;
@@ -79,32 +86,26 @@ let idb = new function () {
         this.save = function(store, value) {
             return executeQuery('readwrite', store, (t, resolve, reject) => {
                 let obj = t.objectStore(store);
-                obj.put(value);
+                let req = obj.put(value);
 
-                t.addEventListener('complete', e => resolve());
-                t.addEventListener('abort', e => logAndReject(reject, 'Database transaction failure'));
+                req.onsuccess = e => resolve();
+                req.onerror = e => logAndReject(reject, 'Failed to save record');
             });
         };
 
         this.saveWithKey = function(store, key, value) {
             return executeQuery('readwrite', store, (t, resolve, reject) => {
                 let obj = t.objectStore(store);
-                obj.put(value, key);
+                let req = obj.put(value, key);
 
-                t.addEventListener('complete', e => resolve());
-                t.addEventListener('abort', e => logAndReject(reject, 'Database transaction failure'));
+                req.onsuccess = e => resolve();
+                req.onerror = e => logAndReject(reject, 'Failed to save record');
             });
         };
 
-        this.saveAll = function(store, values) {
-            return executeQuery('readwrite', store, (t, resolve, reject) => {
-                let obj = t.objectStore(store);
-                for (let value of values)
-                    obj.put(value);
-
-                t.addEventListener('complete', e => resolve());
-                t.addEventListener('abort', e => logAndReject(reject, 'Database transaction failure'));
-            });
+        this.saveAll = async function(store, values) {
+            for (let value of values)
+                await self.save(store, value);
         };
 
         this.load = function(where, key) {

commit 322df959437121a6cf9e284e9f33f2ca0af6e0a2

Fix inactive transaction errors introduced in c7626e9

src/web/libjs/indexedDB.js
@@ -60,13 +60,20 @@ let idb = new function () {
             let p = new Promise((resolve, reject) => {
                 transaction = db.transaction(stores, mode);
 
-                transaction.addEventListener('complete', e => resolve());
-                transaction.addEventListener('abort', e => reject(new Error('Database transaction failure')));
+                transaction.oncomplete = e => resolve();
+                transaction.onabort = e => reject(new Error('Database transaction aborted'));
+                transaction.onerror = e => {
+                    e.preventDefault();
+                    reject(new Error(transaction.error));
+                };
             });
 
             try {
                 await func();
-                await p;
+
+                // Don't await here; We need to execute the finally clause before we let
+                // the user execute any other transaction!
+                return p;
             } catch (err) {
                 if (!aborted && transaction.error == null)
                     transaction.abort();