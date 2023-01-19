7aded62e72b080fcd9a7c9313881f2a06d9cdfbc
Niels Martignène <niels.martignene@protonmail.com>
2023-01-19 12:04:16 +0100

    Update Goupile electron client to working version

--- src/goupile/electron/app/index.js
+++ src/goupile/electron/app/index.js
@@ -9,39 +9,6 @@ const { autoUpdater } = require('electron-updater');
 if (app.requestSingleInstanceLock()) {
     let win = null;
 
-    ipcMain.on('get_usb_key', (e, id) => {
-        let drives = 'DEFGHIJKLMNOPQRSTUVWXYZ';
-
-        for (let drive of drives) {
-            let filename = `${drive}:/Goupile/${id}`;
-
-            if (fs.existsSync(filename)) {
-                let key = fs.readFileSync(filename, { encoding: 'ascii' });
-                key = key.trim();
-
-                e.returnValue = key;
-                return;
-            }
-        }
-
-        e.returnValue = null;
-    });
-
-    ipcMain.on('has_usb_key', (e, id) => {
-        let drives = 'DEFGHIJKLMNOPQRSTUVWXYZ';
-
-        for (let drive of drives) {
-            let filename = `${drive}:/Goupile/${id}`;
-
-            if (fs.existsSync(filename)) {
-                e.returnValue = true;
-                return;
-            }
-        }
-
-        e.returnValue = false;
-    })
-
     app.on('ready', async () => {
         if (isPackaged())
             autoUpdater.checkForUpdatesAndNotify();
@@ -54,15 +21,7 @@ if (app.requestSingleInstanceLock()) {
             }
         });
 
-        let menu = Menu.buildFromTemplate(Menu.getApplicationMenu().items);
-        menu.insert(menu.items.length - 1, new MenuItem({
-            label: 'Print',
-            click: () => win.webContents.print({
-                silent: false,
-                printBackground: true,
-            })
-        }));
-        win.setMenu(menu);
+        customizeMenu(win);
 
         win.loadURL(settings.homepage);
     });
@@ -76,6 +35,24 @@ if (app.requestSingleInstanceLock()) {
     });
 } else {
     app.quit()
+}
+
+function customizeMenu(win) {
+    let menu = Menu.buildFromTemplate(Menu.getApplicationMenu().items);
+
+    menu.insert(menu.items.length - 1, new MenuItem({
+        label: 'Print',
+        click: () => win.webContents.print({
+            silent: false,
+            printBackground: true,
+        })
+    }));
+    win.setMenu(menu);
+
+    win.webContents.on('did-create-window', (win, details) => {
+        console.log('z');
+        customizeMenu(win);
+    });
 }
 
 function isPackaged() {
